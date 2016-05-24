6b0bd81fc7bc6accd65388652eecc5b2ef2ce790
Evgeny Barinov <z.barinov@gmail.com>
2016-05-24 22:44:21 +0300

    crud

---
+++ src/TestPostBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TestPostBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TestPostBundle:Default:index.html.twig');
+    }
+}
---
+++ src/TestPostBundle/TestPostBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TestPostBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestPostBundle extends Bundle
+{
+}
